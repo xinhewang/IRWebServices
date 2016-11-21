f87adf98144ec52567ec90a978283d9964a06e34
xinhe <xinhehe8612@gmail.com>
2016-11-20 18:33:49 -0800

    api to find all Nouns

--- MainHandler.py
+++ MainHandler.py
@@ -1,17 +1,28 @@
 import tornado.ioloop
 import tornado.web
 
+import json
+from NLTKParser import NLTKParser
+Parser = NLTKParser()
+
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("hellow, world")
+	def post(self):
+		out = {"terms":[]}
+		try:
+			data = json.loads(self.request.body)
+			response = Parser.NounsParser(data["sentence"])
+			out["terms"] = response
+			self.write(out)
+		except ValueError, e:
+			self.write(out)
+		
 
 def make_app():
-    return tornado.webApplication([
-
-        (r"/", MainHandler),
-    ])
+	return tornado.web.Application([
+		(r"/", MainHandler),
+	])
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+	app = make_app();
+	app.listen(8888)
+	tornado.ioloop.IOLoop.current().start()
--- NLTKParser.py
+++ NLTKParser.py
@@ -1,7 +1,7 @@
 from nltk.tag import pos_tag
 
 class NLTKParser():
-    def NoneParser(sentence):
+    def NounsParser(self, sentence):
         tagged_sent = pos_tag(sentence.split())
         propernouns = [word for word,pos in tagged_sent if pos == 'NNP']
         return propernouns
